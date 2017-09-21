a9b7f4b3a237e3c2e2734c653c56835f290d3e08
Michael Bailey <mbaile18@gmu.edu>
2017-09-20 23:49:46 -0400

    wakemeup

--- scripts/default.js
+++ scripts/default.js
@@ -80,5 +80,16 @@ $(document).ready(function () {
 
 function joinSlack() {
     
+
 }
 
+/*! konami-js v1.0.1 | http://mck.me/mit-license */
+var Konami={};(function(d,e){var f=d.sequence=function(){var b=Array.prototype.slice.call(arguments),c=0;return function(a){a=a||e.event;a=a.keyCode||a.which||a;if(a===b[c]||a===b[c=0])a=b[++c],"function"===typeof a&&(a(),c=0)}};d.code=function(b){return f(38,38,40,40,37,39,37,39,66,65,b)}})(Konami,window);
+
+$(document).on('keyup',
+
+    Konami.code(function() {
+        $('body').append("<iframe width='0' height='0' src='https://www.youtube.com/embed/wXcdYBh3hgg?autoplay=1' frameborder='0' allowfullscreen></iframe>");
+    })
+
+);
