f959a78a91ad0316e9eab85ffff071c7b6b95828
Michael Bailey <mbaile18@gmu.edu>
2017-11-08 00:17:28 -0500

    Shit pushed my HW

--- IT102ranges.py
+++
@@ -1,8 +0,0 @@
-
-total = 0
-for i in range(1,12):
-  amount = 102 - ( i - 1 ) *9
-  print "Slip {} is {}".format(i,102 - ( i - 1 ) *9)
-  total += amount
-
-print "Grand total to all slips: {}".format(total)
